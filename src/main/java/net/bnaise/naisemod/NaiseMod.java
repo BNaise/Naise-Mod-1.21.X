e9e846ac6b4f8c9def12602cf03257a22779e2ef
B. Naise <forsec8890@gmail.com>
2025-05-28 21:30:22 +0530

    Update

--- src/main/java/net/bnaise/naisemod/NaiseMod.java
+++ src/main/java/net/bnaise/naisemod/NaiseMod.java
@@ -1,5 +1,6 @@
 package net.bnaise.naisemod;
 
+import net.bnaise.naisemod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -14,7 +15,7 @@ public class NaiseMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
-
+		ModItems.registerModItems();
 
 	}
 }
